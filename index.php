99195f7aca076867aee313acfb7faee2c92cb503
julioenred <yalach@gmail.com>
2017-10-17 14:50:58 +0200

    update index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <form method='POST' action='"https://sis-t.redsys.es:25443/sis/realizarPago'>
+
+    </form>
+
+</body>
+</html>
