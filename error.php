a0aafb32fb9a11e04694120e0559b24796d8bb3b
julioenred <julioperezdecastro@gmail.com>
2017-10-31 14:18:32 +0100

    change error.php

--- error.php
+++ error.php
@@ -1,3 +1,30 @@
 <?php 
 
-echo 'algo salió mal';
+include './redsys/apiRedsys.php';
+include 'config.php';
+
+$redsys = new RedsysAPI;
+
+if (!empty( $_GET ) ) 
+{             
+    $version = $_GET["Ds_SignatureVersion"];
+    $datos = $_GET["Ds_MerchantParameters"];
+    $signatureRecibida = $_GET["Ds_Signature"];
+    
+    $decodec = $redsys->decodeMerchantParameters($datos);    
+    $kc = KEY_DEV;
+    $firma = $redsys->createMerchantSignatureNotif($kc, $datos);  
+
+    if ($firma === $signatureRecibida)
+    {
+        echo '<pre>';
+        var_dump($decodec);
+        echo '</pre>';
+    } 
+    else 
+    {
+        echo '<pre>';
+        var_dump($decodec);
+        echo '</pre>';
+    }
+}
