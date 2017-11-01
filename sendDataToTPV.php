975cb8d9d911f506c8c7e8d3358905db89297fdc
julioenred <yalach@gmail.com>
2017-11-01 23:19:40 +0100

    request curl v6

--- sendDataToTPV.php
+++ sendDataToTPV.php
@@ -36,20 +36,16 @@
     $signature = $redsys->createMerchantSignature($key);
 
     // abrimos la sesión cURL
-    $ch = curl_init();
+    $ch = curl_init(REQUEST_DEV);
      
-    // definimos la URL a la que hacemos la petición
-    curl_setopt($ch, CURLOPT_URL, REQUEST_DEV);
     // indicamos el tipo de petición: POST
-    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POST, 1);
     // definimos cada uno de los parámetros
     curl_setopt($ch, CURLOPT_POSTFIELDS, "Ds_SignatureVersion=$version&Ds_MerchantParameters=$params&Ds_Signature=$signature");
      
     // recibimos la respuesta y la guardamos en una variable
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $remote_server_output = curl_exec ($ch);
-
-    // var_dump($remote_server_output);
+    curl_exec ($ch);
      
     // cerramos la sesión cURL
     curl_close ($ch);
